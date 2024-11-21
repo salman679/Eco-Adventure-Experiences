ce61ca927d9c7a5124622f1ceb9b7dc382053286
Salman Izhar <Sbinazhar671@gmail.com>
2024-11-21 12:37:00 +0600

    forget password feture has been added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -50,7 +50,7 @@ const Header = () => {
               <div
                 tabIndex={0}
                 role="button"
-                className="btn btn-ghost btn-circle avatar"
+                className="btn  btn-ghost btn-circle avatar"
               >
                 <div className="w-10 rounded-full">
                   <img
